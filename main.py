ca234397c0025f178c9f4cfabbb44e0a9f37d249
Hegyi Norbert <90178531+hnorbi1@users.noreply.github.com>
2021-12-13 08:23:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+2. Feladat
+Készíts egy programot, ami bekér egy számot a felhasználótól, majd kiírja, hogy a megadott szám páros-e vagy páratlan!
+
+[Tipp] A maradékos osztás segít! Mennyivel is kell elosztanod a számot maradékosan, hogy kiderüljön páros-e? Ebben az esetben mennyi lesz a maradék?
+'''
+szam = int(input('Kérek egy számot!'))
+if szam == 8:
+  print('A megadott szám páros')
+elif szam == 6:
+    print('A megadott szám páratlan')
